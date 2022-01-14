c356e5ab1b6d17058b7bb30ae486d151f5d70185
Maria Reedstrom <maria.reedstrom@gmail.com>
2022-01-14 11:31:28 -0600

    commit tree d3859541e5e2abc5e42fb13765024792f6cf9ded

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import react from "react";
+import EyesOnMe from "./EyesOnMe";
+import Keypad from "./Keypad";
+
+function App() {
+  return (
+    <div>
+      <Keypad />
+      <EyesOnMe />
+    </div>
+  );
+}
